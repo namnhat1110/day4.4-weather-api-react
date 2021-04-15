1ecfad3fe8634dea31b5de7d88066b3067a0cf43
NhatNam <namnhat1110@gmail.com>
2021-04-15 16:36:55 +0700

    innitial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
+// import { Tab, Row, Col, Nav, TabContainer } from 'react-bootstrap';
 import './App.css';
+import WeatherInfo from "./component/WeatherInfo.js";
+const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
 
 function App() {
+  const [weather, setWeather] = useState(null);
+
+  const getWeatherByCurrentLocation = async (lat, lon) => {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`)
+    console.log({ response });
+    const weatherData = await response.json();
+    setWeather(weatherData)
+  }
+
+  useEffect(() => {
+    const getWeatherByCurrentLocation = async (lat, lon) => {
+      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`)
+      console.log({ response });
+      const weatherData = await response.json();
+      setWeather(weatherData)
+    }
+
+
+    navigator.geolocation.getCurrentPosition((position) => {
+      const { latitude, longitude } = position.coords;
+      getWeatherByCurrentLocation(latitude, longitude);
+    });
+  }, []);
+
+  console.log(weather)
+
+
+  if (!weather) {
+    return <h1>Loading</h1>
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Weather App</h1>
+      <WeatherInfo weather={weather} />
+      <div>
+        <button onClick={() => getWeatherByCurrentLocation(21.0278, 105.8342)}>Hanoi</button>
+        <button onClick={() => getWeatherByCurrentLocation(40.712667022068445, -74.00670067020043)}>NewYork</button>
+
+      </div>
     </div>
   );
 }
+
+
+
 
 export default App;
